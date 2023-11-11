8f89675959ba27350c1e299082f951381995d386
erickarodoom <erickarodom@gmail.com>
2023-11-10 17:37:18 -0800

    task list working, need to work on completion and delete functionality

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const toDoListItems = [];
-const newToDo =  document.getElementById('task-input').value;
+// Global Variables
+const x = document.getElementById("task-input");
 
-console.log('ericka');
+function test(){
+const newTask = x.value;
+const item = document.createElement("li");
+const completeBtn = document.createElement("button");
+const completeBtnText = document.createTextNode("Done");
+const deleteBtn = document.createElement("button");
+const deleteBtnText = document.createTextNode("Delete");
+const node = document.getElementById("list-ctnr").appendChild(item);
+
+deleteBtn.appendChild(deleteBtnText);
+completeBtn.appendChild(completeBtnText);
+node.innerHTML = newTask;
+node.appendChild(completeBtn);
+node.appendChild(deleteBtn);
+x.value= " ";
+
+}
+
+function taskDone(){
+    
+}
 
