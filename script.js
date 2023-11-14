51792a772c1e315e6f61e85f287381943ba48bbc
erickarodoom <erickarodom@gmail.com>
2023-11-13 16:21:04 -0800

    mvp working
    need to correct delete button logic still

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 // Global Variables
 const x = document.getElementById("task-input");
+const addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", toDo);
 
-function test(){
-const newTask = x.value;
-const item = document.createElement("li");
-const completeBtn = document.createElement("button");
-const completeBtnText = document.createTextNode("Done");
-const deleteBtn = document.createElement("button");
-const deleteBtnText = document.createTextNode("Delete");
+
+function toDo(){
+    const newTask = x.value;
+    if( newTask == " "){
+        alert("Enter a task first, to add to the list. Thanks");
+    } else {
+    
+    const newTaskText = document.createElement("p");
+    newTaskText.innerHTML = newTask
+    const item = document.createElement("li");
+    item.appendChild(newTaskText);
+
+    // Start of completed task button
+    const completeBtn = document.createElement("button");
+    const completeBtnText = document.createTextNode("Done");
+    completeBtn.appendChild(completeBtnText);
+    item.appendChild(completeBtn);
+
+    // toggle strike on/off for completed task.
+    completeBtn.addEventListener("click", function(){
+        const addStrike = newTaskText.classList.toggle("task-done");
+        // Add delete button 
+        const deleteBtn = document.createElement("button");
+        const deleteBtnText = document.createTextNode("Delete");
+        deleteBtn.appendChild(deleteBtnText);
+        item.appendChild(deleteBtn);
+       
+        //delete "this" to do on click
+        deleteBtn.addEventListener("click", function(){
+        item.remove();
+    });
+});
+
+// add task to list
 const node = document.getElementById("list-ctnr").appendChild(item);
 
-deleteBtn.appendChild(deleteBtnText);
-completeBtn.appendChild(completeBtnText);
-node.innerHTML = newTask;
-node.appendChild(completeBtn);
-node.appendChild(deleteBtn);
+//clear value from input
 x.value= " ";
-
+ }
 }
-
-function taskDone(){
-    
-}
-
